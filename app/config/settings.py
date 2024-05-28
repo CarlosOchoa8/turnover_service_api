ae648c0fa33719d8396a9aeda3b0b3e77b97d2ee
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2024-05-27 19:23:01 -0600

    add: ml model and csv
    add: database config
    
    add: crud_base
    
    add: collaborator migration

---
+++ app/config/settings.py
@@ -0,0 +1,28 @@
+"""
+Settings for the project.
+"""
+
+from pydantic import Extra
+from pydantic_settings import BaseSettings
+
+from app.config.common import CONFIG_FILE, AppSettings
+from app.config.database.settings import DatabaseSettings
+
+
+class Settings(BaseSettings):
+    """
+    Settings class for the project.
+    """
+    app_environment: str
+    database: AppSettings = DatabaseSettings()
+
+    class Config:
+        """
+        Settings class configuration.
+        """
+
+        env_file = CONFIG_FILE
+        extra = "allow"
+
+
+settings = Settings()
---
+++ db/migrations/20240527224431_create.sql
@@ -0,0 +1,38 @@
+-- migrate:up
+CREATE TABLE "collaborator"
+(
+   id integer generated always as identity PRIMARY KEY,
+   age integer not null,
+   attrition varchar(255),
+   business_travel varchar(255) not null,
+   daily_rate integer not null,
+   department varchar(255) not null,
+   distance_from_home integer not null,
+   education integer not null,
+   education_field varchar(255) not null,
+   employee_number integer not null,
+   environment_satisfaction integer not null,
+   gender varchar(255) not null,
+   hourly_rate integer not null,
+   job_involvement integer not null,
+   job_role varchar(255) not null,
+   job_satisfaction integer not null,
+   marital_status varchar(255) not null,
+   monthly_income integer not null,
+   monthly_rate integer not null,
+   num_companies_worked integer not null,
+   over_time varchar(255) not null,
+   percent_salary_hike integer not null,
+   performance_rating integer not null,
+   relationship_satisfaction integer not null,
+   stock_option_level integer not null,
+   training_times_last_year integer not null,
+   work_life_balance integer not null,
+   years_at_company integer not null,
+   years_in_current_role integer not null,
+   years_since_last_promotion integer not null,
+   years_with_curr_manager integer not null
+);
+
+-- migrate:down
+DROP TABLE "collaborator"
