bc7d4946b47401ed9c604651ae1b02ecff75ac92
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2024-05-29 12:57:05 -0600

    refactor: comment innecesary code

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,28 +1,28 @@
-"""
-Settings for the project.
-"""
+# """
+# Settings for the project.
+# """
 
-from pydantic import Extra
-from pydantic_settings import BaseSettings
+# from pydantic import Extra
+# from pydantic_settings import BaseSettings
 
-from app.config.common import CONFIG_FILE, AppSettings
-from app.config.database.settings import DatabaseSettings
-
-
-class Settings(BaseSettings):
-    """
-    Settings class for the project.
-    """
-    app_environment: str
-    database: AppSettings = DatabaseSettings()
-
-    class Config:
-        """
-        Settings class configuration.
-        """
-
-        env_file = CONFIG_FILE
-        extra = "allow"
+# from app.config.common import CONFIG_FILE, AppSettings
+# from app.config.database.settings import DatabaseSettings
 
 
-settings = Settings()
+# class Settings(BaseSettings):
+#     """
+#     Settings class for the project.
+#     """
+#     app_environment: str
+#     database: AppSettings = DatabaseSettings()
+
+#     class Config:
+#         """
+#         Settings class configuration.
+#         """
+
+#         env_file = CONFIG_FILE
+#         extra = "allow"
+
+
+# settings = Settings()
