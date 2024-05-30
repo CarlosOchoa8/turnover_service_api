a43684a4c2128aa5cb8ea7e4673ab8af9ee7efe9
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2024-05-29 20:30:21 -0600

    tests: test cases for endpoints added

---
+++ tests/endpoints/test_endpoints.py
@@ -0,0 +1,51 @@
+from tests.conftest import client
+from tests.endpoints.mocks import (collaborator_data, collaborator_id,
+                                   collaborator_id_fake)
+
+
+def test_post_method():
+    """
+    Test the POST method for creating a new collaborator.
+    Returns: collaborator object stored in db
+    """
+    response = client.post(
+        "api/v1/collaborators/", json=collaborator_data)
+
+    assert response.status_code == 200
+    assert '"employee_number":12345' in response.text
+
+
+def test_get_method():
+    """
+    Test the POST method for creating a new collaborator.
+    Returns: collaborator object stored in db
+    """
+    response = client.get(
+        f"api/v1/collaborators/score/{collaborator_id}")
+    assert response.status_code == 200
+    assert '"employee_number":12345' in response.text
+
+
+def test_existent_collaborator_in_post_method():
+    """
+    Test the POST method for creating a new collaborator.
+    Returns: collaborator object stored in db
+    """
+    response = client.post(
+        "api/v1/collaborators/", json=collaborator_data)
+    print("TEXTOOOOOOOOOOOOOOOOOOOOTEXTOOOOOOOOOOOOOOOOOOOOTEXTOOOOOOOOOOOOOOOOOOOOTEXTOOOOOOOOOOOOOOOOOOOO")
+    print(response.text)
+    assert response.status_code == 200
+    assert '{"message":"Collaborator 12345 is already registered.","collaborator id":12345,"collaborator score":0.3249136199461228}' in response.text
+
+
+def test_not_collaborator_retrieved_from_get_method():
+    """
+    Test the POST method for creating a new collaborator.
+    Returns: collaborator object stored in db
+    """
+    response = client.get(
+        f"api/v1/collaborators/score/{collaborator_id_fake}")
+
+    assert response.status_code == 404
+    assert f'Collaborator {collaborator_id_fake} does not have record.' in response.text
---
+++ tests/mocks/collaborator_mock.py
@@ -0,0 +1,41 @@
+from app.schemas import CollaboratorCreateSchema
+
+collaborator_data =  {
+                "age": 30,
+                "attrition": "No",
+                "business_travel": "Travel_Rarely",
+                "daily_rate": 750,
+                "department": "Sales",
+                "distance_from_home": 10,
+                "education": 4,
+                "education_field": "Marketing",
+                "employee_count": 1,
+                "employee_number": 12345,
+                "environment_satisfaction": 3,
+                "gender": "Male",
+                "hourly_rate": 35,
+                "job_involvement": 3,
+                "job_level": 2,
+                "job_role": "Sales Executive",
+                "job_satisfaction": 4,
+                "marital_status": "Married",
+                "monthly_income": 6000,
+                "monthly_rate": 12000,
+                "num_companies_worked": 2,
+                "over_18": "Y",
+                "over_time": "Yes",
+                "percent_salary_hike": 15,
+                "performance_rating": 3,
+                "relationship_satisfaction": 2,
+                "standard_hours": 8,
+                "stock_option_level": 1,
+                "total_working_years": 10,
+                "training_times_last_year": 2,
+                "work_life_balance": 3,
+                "years_at_company": 5,
+                "years_in_current_role": 3,
+                "years_since_last_promotion": 1,
+                "years_with_curr_manager": 2
+}
+
+collaborator_in = CollaboratorCreateSchema(**collaborator_data)
